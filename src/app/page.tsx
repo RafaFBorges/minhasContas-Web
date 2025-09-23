fddbff50df1cfb8a32a31cab01c36977c6569ba7
Rafael Fernandes Borges <46011419+RafaFBorges@users.noreply.github.com>
2025-09-23 16:27:22 -0300

    [feature] CRUD Expenses in server #WEB-3 (#7)
    * [feature] Fetch info in server at inicialization #WEB-3
    
    * [feature] mapping recieved expense data to cards  #WEB-3
    
    * [feature] creating expense in server + card at response #WEB-3
    
    * [feature] deleting a expense in local + server #WEB-3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,105 @@
 'use client'
 
-import React, { useState } from 'react'
-import { FaPlus } from "react-icons/fa"
+import { Expense } from '@/domain/Expense'
+import React, { useEffect, useState } from 'react'
+import { FaPlus as AddIcon, FaTrash as DeleteIcon } from "react-icons/fa"
+
+
+const SERVER_PATH = 'https://minhascontas-server.onrender.com/'
+const EXPENSES_ENDPOINT = 'expense'
+
+export async function handleGET(endpoint: string) {
+  try {
+    console.log("handleGET : [start] endpoint=" + SERVER_PATH + endpoint)
+
+    const response = await fetch(SERVER_PATH + endpoint, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+
+    if (!response.ok)
+      throw new Error("Erro HTTP: " + response.status)
+
+    const data = await response.json()
+
+    let logMessage = "handleGET : [request send]"
+    if (!data)
+      logMessage += 'empty data'
+    else if (Array.isArray(data))
+      logMessage += 'Count=' + data.length
+    else if (typeof data === "object")
+      logMessage += 'ObjectKeysCount=' + Object.keys(data).length
+    else
+      logMessage += 'Unexpected response type'
+    console.log(logMessage)
+
+    return data
+  } catch (err) {
+    return Response.json({ error: "Falha ao buscar dados" }, { status: 500 })
+  }
+}
+
+export async function handlePOST(endpoint: string, body: object) {
+  try {
+    console.log("handlePOST : [start] endpoint=" + SERVER_PATH + endpoint)
+
+    const response = await fetch(SERVER_PATH + endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    })
+
+    if (!response.ok)
+      throw new Error("Erro HTTP: " + response.status)
+
+    const data = await response.json()
+
+    let logMessage = "handlePOST : [request send]"
+    if (!data)
+      logMessage += 'empty data'
+    else if (Array.isArray(data))
+      logMessage += 'Count=' + data.length
+    else if (typeof data === "object")
+      logMessage += 'ObjectKeysCount=' + Object.keys(data).length
+    else
+      logMessage += 'Unexpected response type'
+    console.log(logMessage)
+
+    return data
+  } catch (err) {
+    return Response.json({ error: "Erro ao processar" }, { status: 400 });
+  }
+}
+
+export async function handleDELETE(endpoint: string) {
+  try {
+    console.log("handleDELETE : [start] endpoint=" + SERVER_PATH + endpoint)
+
+    const response = await fetch(SERVER_PATH + endpoint, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+
+    console.log('handleDELETE : status=' + response.status)
+
+    if (!response.ok)
+      throw new Error("Erro HTTP: " + response.status)
+
+    return response.status == 204
+  } catch (err) {
+    return Response.json({ error: "Erro ao processar" }, { status: 400 });
+  }
+}
 
 export default function Home() {
   const [value, setValue] = useState<number>(0)
-  const [valueList, setValueList] = useState<number[]>([])
+  const [valueList, setValueList] = useState<Expense[]>([])
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const parsed: number = parseFloat(e.target.value)
@@ -14,9 +108,43 @@ export default function Home() {
       setValue(parsed)
   }
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setValueList([...valueList, value])
+  const handleAddNewClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    const response = await handlePOST(EXPENSES_ENDPOINT, {
+      "value": value,
+      "date": new Date()
+    })
+
+    if (response != null)
+      setValueList([...valueList, new Expense(response.id, response.value, response.dates)])
   }
+
+  const handleDeleteClick = async (index: number) => {
+    if (await handleDELETE(EXPENSES_ENDPOINT + '/' + index))
+      setValueList(valueList.filter(expense => {
+        return expense.id !== index;
+      }))
+  }
+
+  async function SyncExpenses() {
+    try {
+      console.log("HOME.useEffect : [initial load] fetching expenses")
+
+      const serverExpensesList: Promise<any[]> = await handleGET(EXPENSES_ENDPOINT)
+
+      if (!(serverExpensesList != null) || !Array.isArray(serverExpensesList))
+        throw Error('Invalid Expense response')
+
+      const expensesList: Expense[] = []
+      serverExpensesList.forEach(expense => expensesList.push(new Expense(expense.id, expense.value, expense.dates)))
+      setValueList(expensesList)
+    } catch (err) {
+      console.error("HOME.useEffect : [Error] erro=", err)
+    }
+  }
+
+  useEffect(() => {
+    SyncExpenses()
+  }, [])
 
   return <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
     <h1>Minhas Contas</h1>
@@ -25,6 +153,7 @@ export default function Home() {
     <div style={styles.flexRow}>
       <input
         type="number"
+        name="expenseValue"
         step="any"
         value={value}
         onChange={handleChange}
@@ -33,18 +162,26 @@ export default function Home() {
       />
 
       <button
-        onClick={handleClick}
+        onClick={handleAddNewClick}
         style={styles.button}
       >
-        <FaPlus />
+        <AddIcon />
       </button>
     </div>
 
-    {valueList != null && valueList.map((value, index) => {
+    {valueList != null && valueList.map((item, index) => {
       return <div style={styles.card} key={index}>
         <div style={styles.content}>
-          <h6 style={styles.title}>{value}</h6>
-          <p style={styles.description}>date</p>
+          <div style={styles.cardFlexRow}>
+            <h6 style={styles.title}>{item.value}</h6>
+            <button
+              onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleDeleteClick(index)}
+              style={styles.cardIcon}
+            >
+              <DeleteIcon />
+            </button>
+          </div>
+          <p style={styles.description}>{item.lastDate}</p>
         </div>
       </div>
     })}
@@ -92,7 +229,6 @@ const styles: { [key: string]: React.CSSProperties } = {
     padding: '1rem',
   },
   title: {
-    margin: '0 0 0.5rem 0',
     fontSize: '1.25rem',
   },
   description: {
@@ -111,5 +247,23 @@ const styles: { [key: string]: React.CSSProperties } = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  cardIcon: {
+    color: '#0070f3',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardFlexRow: {
+    display: 'flex',
+    margin: '0 0 0.5rem 0',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: '350px',
+    boxSizing: 'border-box',
   },
 };
