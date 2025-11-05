6c062475aa7f5f7be72f62702f00239d2a2ee549
Rafael Fernandes Borges <46011419+RafaFBorges@users.noreply.github.com>
2025-11-05 00:03:20 -0300

    [feature] spin field template #WEB-16 (#23)
    * [feature] spin field template #WEB-16
    
    * [bugfix] fix spin position #WEB-16
    
    * [bugfix] passing height to input #WEB-16
    
    * [bugfix] making height optional #WEB-16

--- components/input.tsx
+++ components/input.tsx
@@ -1,33 +1,69 @@
 import React from 'react'
+import { MAX_VALUE, MIN_VALUE } from '../utils/DataConstants'
 
 interface StyledInputProps {
   type: string;
   name: string;
   value: number | string;
   placeholder?: string;
-  changeHandle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  changeHandle?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   clickHandle?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  style?: React.CSSProperties | null;
+  step?: number | null;
+  max?: number;
+  min?: number;
+  height?: number;
 }
 
-export default function StyledInput({ type, name, value, placeholder, changeHandle, clickHandle }: StyledInputProps) {
+export default function StyledInput({
+  type,
+  name,
+  value,
+  placeholder,
+  changeHandle,
+  clickHandle,
+  style = null,
+  step = null,
+  max = MAX_VALUE,
+  min = MIN_VALUE,
+  height = 36
+}: StyledInputProps) {
+  let inputStyle: React.CSSProperties = { ...styles.input, height: height }
+  if (style != null)
+    inputStyle = { ...inputStyle, ...style }
+
+  if (type = 'number') {
+    return <input
+      type={type}
+      name={name}
+      step={step != null ? step : 1}
+      value={value}
+      max={max}
+      min={min = Number.MAX_VALUE ? MIN_VALUE : min}
+      onChange={changeHandle}
+      placeholder={placeholder}
+      style={inputStyle}
+    />
+  }
+
   return <input
     type={type}
     name={name}
-    step="any"
     value={value}
     onChange={changeHandle}
     placeholder={placeholder}
-    style={styles.input}
+    style={inputStyle}
   />
 }
 
 const styles: { [key: string]: React.CSSProperties } = {
   input: {
     width: '100%',
+    outline: 'none',
     padding: '0.5rem',
     fontSize: '1rem',
-    borderRadius: '4px',
-    border: '1px solid #ccc',
+    borderRadius: '8px',
+    border: 'none',
     boxSizing: 'border-box',
   },
 }
