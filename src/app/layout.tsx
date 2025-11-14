b856450ee353fcc863ac20721a774ddb0622a000
Rafael <rafaeelfernandes@hotmail.com>
2025-11-14 02:13:26 -0300

    [bugfix] remove wrong code #WEB-17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import getCookie from './actions/cookiesManager'
 import { ModalProvider } from '../../utils/hook/modalHook'
 import { ThemeProvider } from '../../utils/hook/themeHook'
 import { TranslateProvider } from '../../utils/hook/translateHook'
-import { MouseProvider } from '../../utils/hook/mouseHook'
 import { LANG_KEY, THEME_KEY } from '../../utils/DataConstants'
 
 export const metadata: Metadata = {
@@ -22,13 +21,11 @@ export default async function RootLayout({
   const initialTheme = await getCookie(THEME_KEY)
   const initialLanguage = await getCookie(LANG_KEY)
 
-  return <MouseProvider>
-    <TranslateProvider lang={initialLanguage}>
-      <ThemeProvider theme={initialTheme}>
-        <ModalProvider>
-          {children}
-        </ModalProvider>
-      </ThemeProvider>
-    </TranslateProvider>
-  </MouseProvider>
+  return <TranslateProvider lang={initialLanguage}>
+    <ThemeProvider theme={initialTheme}>
+      <ModalProvider>
+        {children}
+      </ModalProvider>
+    </ThemeProvider>
+  </TranslateProvider>
 }
